84dbc27ea79adb0b44a8503fd4b48e86b148d586
Grigoriy A. Malyshev <greeshaa@gmail.com>
2013-08-15 00:05:38 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ElevatorTools::Application.config.secret_token = 'd9acfdaaf2de4eeb8aae0ac0cc45bfbf5c44d17ac9831e61cf537c10ac6de09f479361becf2c6f3c81b6a209e5e995c65b7880aa89185ff3a3416f3624cc500f'
