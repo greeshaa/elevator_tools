ff0dee64fedec202b8a275cd47aa9f4fe0bdea31
Grigoriy A. Malyshev <greeshaa@gmail.com>
2013-10-26 20:53:56 +0700

    format table by bootstrap

--- db/migrate/20130827075551_add_street_id_to_builds.rb
+++
@@ -1,6 +0,0 @@
-class AddStreetIdToBuilds < ActiveRecord::Migration
-  def change
-  	add_column :builds, :street_id, :integer
-  	add_index :builds, :street_id
-  end
-end
--- db/migrate/20130921103611_create_lifts.rb
+++
@@ -1,8 +0,0 @@
-class CreateLifts < ActiveRecord::Migration
-  def change
-    create_table :lifts do |t|
-
-      t.timestamps
-    end
-  end
-end
