2d1c6895373be186458f4d377046a9a6118b143a
Grigoriy A. Malyshev <greeshaa@gmail.com>
2014-04-30 20:07:25 +0700

    migration fix

--- db/migrate/20131031071224_drop_table_equip_log.rb
+++
@@ -1,6 +0,0 @@
-# -*- encoding : utf-8 -*-
-class DropTableEquipLog < ActiveRecord::Migration
-  def change
-  	drop_table :equip_logs
-  end
-end
--- db/migrate/20131101133446_drop_table_equip_pools.rb
+++
@@ -1,7 +0,0 @@
-# -*- encoding : utf-8 -*-
-class DropTableEquipPools < ActiveRecord::Migration
-
-  def change
-  	drop_table :equip_pools
-  end
-end
