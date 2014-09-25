a00825b5fd88e5315ab55d41c4a25d86cde8ddda
Grigoriy A. Malyshev <greeshaa@gmail.com>
2014-09-25 22:54:52 +0700

    i18n experiments #1

--- config/application.rb
+++ config/application.rb
@@ -38,7 +38,8 @@ module ElevatorTools
     #The default locale is :ru and all translations from config/locales/*.rb,yml are auto loaded.
     #config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ru
-    #config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir["#{Rails.root.to_s}/config/locales/**/*.{rb,yml}"]
+    # config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
