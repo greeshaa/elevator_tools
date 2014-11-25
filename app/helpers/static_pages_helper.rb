7ec9672659e7e2bd0ad6eced6a8666cb0dbd9d51
Grigoriy A. Malyshev <greeshaa@gmail.com>
2014-11-25 20:29:09 +0600

    ????

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -8,7 +8,7 @@ module StaticPagesHelper
          icon: "fa-clock-o",
         count: Inspection.where(next_inspection_at: Date.today..(Date.today + 7.day)).count,
          name: "ПТО",
-         note: "в плане на след. 7 дн.",
+         note: "в плане на 7 дн.",
          link: next_inspections_path
         },
       {
@@ -32,7 +32,7 @@ module StaticPagesHelper
          icon: "fa-check-circle-o",
         count: Inspection.where(created_at: (Date.today - 7.day)..Date.today + 1.day).count,
          name: "ПТО",
-         note: "добавлено за послед. 7 дн.",
+         note: "добавлено за 7 дн.",
          link: last_inspections_path
       },
       {
@@ -48,7 +48,7 @@ module StaticPagesHelper
          icon: "fa-ban",
         count: Lift.overdue_lifts.count,
          name: Russian.p(Lift.overdue_lifts.count, "лифт", "лифта", "лифтов"),
-         note: "с истекшим сроком экспл-и",
+         note: "с истекшим сроком",
          link: overdue_lifts_path
       },
       {
